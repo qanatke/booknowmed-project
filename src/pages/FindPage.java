6e28e965ad7682b5ae76312cb3e9c449e9c484a1
Natalia Peychev <natkebatke@gmail.com>
2020-11-26 19:29:32 +0100

    Find and book method added to FindPage

--- src/pages/FindPage.java
+++ src/pages/FindPage.java
@@ -2,10 +2,9 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FindPage extends BasicPage {
@@ -14,42 +13,51 @@ public class FindPage extends BasicPage {
 		super(driver, js, waiter);
 	}
 
-	public WebElement getTreatment() {
-		return this.driver.findElement(By.xpath(
-				//"/html/body/app-root/ng-component/section[1]/div/bnm-platform-search/div/bnm-custom-select/div[1]"
-				"//section/div/bnm-platform-search/div/bnm-custom-select/div[1]"
-				));
+	protected WebElement getTreatment() {
+		return this.driver.findElement(By.xpath("//section/div/bnm-platform-search/div/bnm-custom-select/div[1]"));
 	}
 
 	protected WebElement getHD() {
-		//return this.driver.findElement(By.xpath("//div[@class='items']/div[1]"));
-		return this.driver.findElement(By.xpath(
-				"//section/div/bnm-platform-search/div/bnm-custom-select/div[2]/div/div[2]/div[1]"));
+		return this.driver.findElement(
+				By.xpath("//section/div/bnm-platform-search/div/bnm-custom-select/div[2]/div/div[2]/div[1]"));
 	}
 
 	protected WebElement getHDF() {
-		//return this.driver.findElement(By.xpath("//div[@class='items']/div[2]"));
-		return this.driver.findElement(By.xpath(
-				"//section/div/bnm-platform-search/div/bnm-custom-select/div[2]/div/div[2]/div[2]"));
+		return this.driver.findElement(
+				By.xpath("//section/div/bnm-platform-search/div/bnm-custom-select/div[2]/div/div[2]/div[2]"));
 	}
 
-	public void setTreatment(String type) throws Exception {
+	protected void setTreatment(String type) {
 		getTreatment().click();
-		
-//		WebElement list = this.driver.findElement(By.xpath("//section/div/bnm-platform-search/div/bnm-custom-select/div[2]"));
-//		waiter.until(ExpectedConditions.visibilityOf(list));
-		
-		
+
 		switch (type) {
 		case "Dialysis HD":
 			this.js.executeScript("arguments[0].click()", getHD());
-			//getHD().click();
 			break;
 		case "Dialysis HDF":
 			this.js.executeScript("arguments[0].click()", getHDF());
-			//getHDF().click();
 			break;
-
 		}
+	}
+
+	protected WebElement getLocation() {
+		return this.driver
+				.findElement(By.xpath("//section/div/bnm-platform-search/div/div[1]/bnm-location-picker/div/input"));
+	}
+
+	protected void setLocation(String location) throws Exception {
+		getLocation().sendKeys(location);
+		Thread.sleep(1000);
+		getLocation().sendKeys(Keys.ENTER);
+	}
+
+	protected WebElement getButton() {
+		return this.driver.findElement(By.xpath("//section/div/bnm-platform-search/div/div[3]/button[1]"));
+	}
+
+	public void findNbookDialysis(String treatment, String location) throws Exception {
+		setTreatment(treatment);
+		setLocation(location);
+		getButton().click();
 	}
 }
